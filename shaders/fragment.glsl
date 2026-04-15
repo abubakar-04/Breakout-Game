9766661648eedd676913531eb5482934203a3102
abubakar-04 <abubakar111980765@gmail.com>
2026-04-15 19:18:31 +0500

    creating the first object

---
+++ shaders/fragment.glsl
@@ -0,0 +1,7 @@
+#version 330 core
+out vec4 FragColor;
+
+void main()
+{
+	FragColor = vec4(0.93, 0.76, 0.25, 1.0);
+}
--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,8 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+#include "Shader.h"
+
 #include <iostream>
 
 const unsigned int SCREEN_WIDTH = 800;
@@ -41,6 +43,41 @@ int main()
 
     glViewport(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
 
+    Shader rectangleShader("shaders/vertex.glsl", "shaders/fragment.glsl");
+
+    float vertices[] = {
+        // positions
+        0.5f, 0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f,
+        -0.5f, -0.5f, 0.0f,
+        -0.5f, 0.5f, 0.0f};
+
+    unsigned int indices[] = {
+        0, 1, 3,
+        1, 2, 3};
+
+    unsigned int VBO;
+    unsigned int VAO;
+    unsigned int EBO;
+
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
     while (!glfwWindowShouldClose(window))
     {
         processInput(window);
@@ -48,9 +85,17 @@ int main()
         glClearColor(0.08f, 0.08f, 0.11f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
+        rectangleShader.use();
+        glBindVertexArray(VAO);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
+
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
 
     glfwTerminate();
     return 0;
